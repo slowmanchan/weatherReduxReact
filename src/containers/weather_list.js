bb04746ab666eeac98f9471e3f0b247827164d79
Norman Chan <slowmanchan@gmail.com>
2018-02-09 18:34:37 -0500

    more work

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+
+  renderWeather(cityData) {
+    const name = cityData.city.name
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className='table table-hover'>
         <thead>
           <tr>
-            <th>
-              City
-            </th>
+            <th>City</th>
+            <th>Temp</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
-          
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
   }
 }
+// {weather} == const weather = state.weather.
+// (state) was the orginal arg
+// pass {weather} to destruct state.weather
+function mapStateToProps({weather}) {
+  return { weather } // {weather} == {weather: weather}
+}
+
+export default connect(mapStateToProps)(WeatherList)
